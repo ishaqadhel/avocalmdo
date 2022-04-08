3d6acefbcf9f4595de074dee0d8d9229856db287
ishaqadhel <49280352+ishaqadhel@users.noreply.github.com>
2022-04-08 15:57:20 +0700

    feat: init xcode project

---
+++ avocalmdo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avocalmdo
+//
+//  Created by Ishaq Adheltyo on 08/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
