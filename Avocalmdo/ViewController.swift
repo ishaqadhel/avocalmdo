38f5e1de3b9f3599414c800643b41e51ae82383a
Mikhael <mikhael_adiputra@yahoo.com>
2022-04-11 14:30:32 +0700

    Initial Commit

---
+++ Avocalmdo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avocalmdo
+//
+//  Created by Mikhael Adiputra on 11/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
